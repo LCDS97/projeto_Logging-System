d529545afc28279ac69a3e91493d4575868befaa
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-06-29 21:32:47 -0300

    Init of project and sequelize implementation

---
+++ server/app.js
@@ -0,0 +1,18 @@
+// ANCHOR Sequelize - server
+const express = require('express');
+const app = express();
+
+const db = require('./models')
+
+// NOTE Definindo que o sequelize ira checar todos as tabelas do banco com os da pasta 'models', quando iniciar a API, caso a tabela não existe ele irá criar com o método sync
+
+db.sequelize.sync().then(() => {
+
+    app.listen(3001, () => {
+        console.log("Server running on port 3001");
+    });
+
+})
+
+
+
