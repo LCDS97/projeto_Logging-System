840766caf6fae72d73586a349af3c35f32f99f27
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-07-05 23:34:24 -0300

    Linkado id dos posts como parametro para rota post

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,20 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "./pages/Home"
 import CreatePost from "./pages/CreatePost"
+import Post from "./pages/Post";
 
 
 function App() {
     return <div className="App">
       <Router>
+        <div className="navbar">
         <Link to="/createpost"> Cria um Post</Link>
         <Link to="/"> Home Page</Link>
+        </div>
         <Switch>
           <Route path="/" exact component={ Home } />
           <Route path="/createpost" exact component={ CreatePost } />
+          <Route path="/post/:id" exact component={ Post } />
         </Switch>
       </Router>
     </div>
