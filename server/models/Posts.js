c7785cf640fe436e7e6bb50733899a726fdd840a
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-07-06 00:41:36 -0300

    Criado relacionamento de 1:m  da tabela Posts com Comments

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -19,5 +19,11 @@ module.exports = (sequelize, DataTypes) => {
         },
     })
 
+
+    Posts.associate = (models) => {
+        Posts.hasMany(models.Comments, {
+            onDelete: "cascade",
+        });
+    }
     return Posts;
 }
