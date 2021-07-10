9e2aa8f1f9d465f5bc6a69c692c5329ffc53a008
LCDS97 <lucas.cds1997@gmail.com>
2021-07-09 23:26:17 -0300

    Deletando comentarios

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { AuthContext } from "../helpers/AuthContext";
 
 function Post() {
   let { id } = useParams();
   const [postObject, setPostObject] = useState({});
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState("");
+  const { authState } = useContext(AuthContext);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
@@ -36,11 +38,24 @@ function Post() {
         if (response.data.error) {
           console.log(response.data.error);
         } else {
-          const commentToAdd = { commentBody: newComment, username: response.data.username };
+          const commentToAdd = {
+            commentBody: newComment,
+            username: response.data.username,
+          };
           setComments([...comments, commentToAdd]);
           setNewComment("");
         }
       });
+  };
+
+  const deleteComment = (id) => {
+    axios.delete(`http://localhost:3001/comments/${id}`, {
+      headers: { accessToken: localStorage.getItem("accessToken") },
+    }).then(() => {
+      setComments(comments.filter((val) => {
+        return val.id !== id;
+      }));
+    });
   };
 
   return (
@@ -69,9 +84,17 @@ function Post() {
           {comments.map((comment, key) => {
             return (
               <div key={key} className="comment">
-                {" "}
                 {comment.commentBody}
                 <label> Usuário: {comment.username}</label>
+                {authState.username === comment.username && (
+                  <button
+                    onClick={() => {
+                      deleteComment(comment.id);
+                    }}
+                  >
+                    X
+                  </button>
+                )}
               </div>
             );
           })}
--- server/models/Users.js
+++ server/models/Users.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
     })
 
 
+
 /*     Users.associate = (models) => {
         Users.hasMany(models.Posts, {
             onDelete: "cascade",
